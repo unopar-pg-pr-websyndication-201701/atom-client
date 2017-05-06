f2376cf187447f16a2d527e334731dc48a3a1e56
ofortesfortes <ofortesfortes@gmail.com>
2017-05-05 23:43:32 -0300

    Add files via upload

--- exibir.php
+++ exibir.php
@@ -17,25 +17,85 @@ if(isset($_GET["q"]) ) {
 }
 $feed = file_get_contents($resultado); 
 $rss = new SimpleXmlElement($feed);
-foreach($rss->channel->item as $entrada) {
 
-	if(strtotime(date("m/d/Y",strtotime($entrada->pubDate)))>= strtotime(date("m/d/Y",strtotime($dataInicial))) && strtotime(date("m/d/Y",strtotime($entrada->pubDate)))<= strtotime(date("m/d/Y",strtotime($dataFinal)))){
+	
+/*	
+if(isset($_GET["q"]) ) {
+	
+		if(strtotime(date("m/d/Y",strtotime($entrada->pubDate)))>= strtotime(date("m/d/Y",strtotime($dataInicial))) && strtotime(date("m/d/Y",strtotime($entrada->pubDate)))<= strtotime(date("m/d/Y",strtotime($dataFinal)))){
 	echo $entrada->title.'<br>';
 	echo $entrada->description.'<br>';
 	echo '<a href='.$entrada->link.'>Clique Aqui para ver completo.</a><br>';
 	echo $entrada->pubDate.'<br><br><br>';
+	
+	
+	
+
+	
 
 }
+else{
+		echo $entrada->title.'<br>';
+	echo $entrada->description.'<br>';
+	echo '<a href='.$entrada->link.'>Clique Aqui para ver completo.</a><br>';
+	echo $entrada->pubDate.'<br><br><br>';
+}
+	
+	
+}
+
+*/
+?>
+
+
+<feed xmlns="http://www.w3.org/2005/Atom">
+
+<title>Home Page — News Feed</title>
+<link href=http://www.meusite.com/>
+<updated>2017-05—05 3:05:47</updated>
+
+<author>
+<name>Autor</name>
+<email>email@exemplo.com</email>
+<uri>http://www.exemplo.com/about—me</uri>
+</author>
+
+<id>http://www.exemplo.com/</id>
+<icon>http://www.coolarchive.com/icons/ico/SPgues31.ico</icon>
+<logo>https://vignette2.wikia.nocookie.net/gtawiki/images/9/9a/PlayStation_1_Logo.png/revision/latest?cb=20100130082645</logo>
+<rights> © 2002—2017 meusite.unopar.com.be </rights>
+<subtitle>Esteja atualizado com o que há de melhor na web</subtitle>
+<category term=Informática/>
+
+<?php
+foreach($rss->channel->item as $entrada) {
+	echo"
+	<entry>
+<title>".$entrada->title."</title>
+<link href='".$entrada->link."'/>
+<id>".$entrada->link."/</id>
+<updated>".$entrada->pubDate."</updated>
+<summary>".$entrada->description."</summary>
+<author>
+ <name>Nome do autor</name>
+</author>
+</entry>
+	
+		
+	
+	
+	";
+	
+	
+	
+}
+?>
+
+
+
+</feed>
     
 
 
 
-   // echo $dataInicial.' '.$dataFinal;
 
-	//echo $entrada->data.'<br>';
-	//echo $entrada->title.'<br>';
-	//echo $entrada->description.'<br>';
-	//echo $entrada->link.'<br>';
-	//echo $entrada->pubDate.'<br><br><br>';
-
-}
